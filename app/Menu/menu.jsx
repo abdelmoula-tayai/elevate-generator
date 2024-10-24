ec1076413dfd7d3f7f9637f5afb662afeb025c9c
Your Name <you@example.com>
2024-10-24 15:40:55 +0200

    Refactor menu and range components

--- app/Menu/menu.jsx
+++ app/Menu/menu.jsx
@@ -1,5 +1,3 @@
-"use client";
-import { useState } from "react";
 import { Range } from "../Range/range";
 
 export const Menu = ({ setImage, image }) => {
--- app/Range/range.jsx
+++ app/Range/range.jsx
@@ -1,9 +1,19 @@
+import { useState } from "react";
+
 export const Range = ({ name, value }) => {
   const [settings, setSettings] = useState({
     padding: 16,
     shadow: 10,
     radius: 16,
   });
+
+  const handleChange = (e) => {
+    setSettings((prevSettings) => ({
+      ...prevSettings,
+      [name]: Number(e.target.value),
+    }));
+  };
+
   return (
     <input
       className="range range-primary w-full my-3"
@@ -12,7 +22,7 @@ export const Range = ({ name, value }) => {
       min={0}
       max={99}
       value={value}
-      onChange={(e) => setSettings(value, Number(e.target.value))}
+      onChange={handleChange}
     />
   );
 };
