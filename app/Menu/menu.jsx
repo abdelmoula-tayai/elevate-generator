a6b23a5c0a7af90cf48f546e2981a71fdfef2a43
Your Name <you@example.com>
2024-10-25 16:41:06 +0200

    Refactor menu component to include image upload functionality. Final result

--- app/Menu/menu.jsx
+++ app/Menu/menu.jsx
@@ -1,25 +1,6 @@
 import { Range } from "../Range/range";
 
-export const Menu = ({ setImage, setSettings }) => {
-  const handleImageUpload = (event) => {
-    const file = event.target.files[0];
-    const reader = new FileReader();
-
-    reader.onloadend = () => {
-      const img = new Image();
-      img.onload = () =>
-        setImage({
-          src: img.src,
-          width: img.width,
-          height: img.height,
-          name: file.name,
-        });
-      img.src = reader.result;
-    };
-
-    reader.readAsDataURL(file);
-  };
-
+export const Menu = ({ handleImageUpload, setSettings }) => {
   return (
     <div className="flex justify-start flex-col bg-slate-800 w-3/4 px-10 py-6 mt-5 rounded-xl shadow-lg">
       <h1 className="font-bold text-2xl mb-5">Settings</h1>
--- app/page.js
+++ app/page.js
@@ -6,37 +6,100 @@ import { renderPNG } from "./render-png";
 
 export default function Home() {
   const [image, setImage] = useState(null);
+
   const [settings, setSettings] = useState({
     padding: 16,
     shadow: 10,
     radius: 16,
   });
 
+  const [loading, setLoading] = useState("idle");
+
+  const handleImageUpload = (event) => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+
+    if (file.type !== "image/png") {
+      alert("We only accept png files");
+      event.target.value = "";
+      return;
+    }
+
+    reader.onloadend = () => {
+      const img = new Image();
+      img.onload = () =>
+        setImage({
+          src: img.src,
+          width: img.width,
+          height: img.height,
+          name: file.name,
+        });
+      img.src = reader.result;
+    };
+
+    reader.readAsDataURL(file);
+  };
+
+  const handleDownload = async (isCopying) => {
+    setLoading(isCopying ? "copying" : "downloading");
+
+    const { blob } = await renderPNG({
+      image,
+      settings,
+    });
+    const url = URL.createObjectURL(blob);
+
+    if (isCopying) {
+      await navigator.clipboard.write([
+        new ClipboardItem({
+          ["image/png"]: blob,
+        }),
+      ]);
+    } else {
+      const a = document.createElement("a");
+      a.href = url;
+      a.download = image.name.replace(".png", "-elevate.png");
+      a.click();
+    }
+
+    setLoading("idle");
+  };
+
   return (
     <div className="flex justify-center items-center flex-col">
-      <Menu setImage={setImage} setSettings={setSettings} />
+      <Menu
+        setImage={setImage}
+        setSettings={setSettings}
+        handleImageUpload={handleImageUpload}
+      />
       <div className="flex justify-center border w-3/4 py-5 mt-4 rounded-md">
         <ImageGenerator settings={settings} image={image} />
       </div>
 
       <button
-        className="btn my-3"
-        disabled={!image}
-        onClick={async () => {
-          const { blob } = await renderPNG({
-            image,
-            settings,
-          });
-
-          const url = URL.createObjectURL(blob);
-
-          const a = document.createElement("a");
-          a.href = url;
-          a.download = image.name.replace(".png", "-elevate.png");
-          a.click();
+        className="btn btn-primary my-3"
+        disabled={!image || loading === "downloading"}
+        onClick={() => {
+          handleDownload(false);
         }}
       >
         Download
+        {loading === "downloading" ? (
+          <span className="loading loading-spinner loading-sm"></span>
+        ) : null}
+      </button>
+
+      <button
+        className="btn btn-active my-3"
+        disabled={!image || loading === "copying"}
+        onClick={() => {
+          handleDownload(true);
+        }}
+      >
+        copy
+        {loading === "copying" ? (
+          <span className="loading loading-spinner loading-sm"></span>
+        ) : null}
       </button>
     </div>
   );
